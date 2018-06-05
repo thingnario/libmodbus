8137b78cb3ab929774b1214ad0f1847e554dfef7
eric11220 <b99902080@ntu.edu.tw>
2018-06-05 08:02:41 +0000

    build_libmodbus.sh need to separately consider local and embedded cases

--- build_libmodbus.sh
+++ build_libmodbus.sh
@@ -20,21 +20,22 @@ ARCH=`echo $item | sed -e 's/-gcc.*//g'`
 echo "Building libmodbus..."
 
 # ======== libmodbus with static build ========
-export ARCH=$ARCH
-export AR=${ARCH}-ar
-export AS=${ARCH}-as
-export LD=${ARCH}-ld
-export RANLIB=${ARCH}-ranlib
-export CC=${ARCH}-gcc
-export NM=${ARCH}-nm
-
 ./autogen.sh;
-if [ $tool_chain_path == '/usr/local'  ]; then
+if [ "$ARCH" == "" ]; then
+  export AR=ar
+  export LD=ld
+  export AS=as
+  export CC=gcc
   ./configure --prefix=$tool_chain_path ac_cv_func_malloc_0_nonnull=yes --enable-static --without-documentation
 else
+  export AR=${ARCH}-ar
+  export AS=${ARCH}-as
+  export LD=${ARCH}-ld
+  export RANLIB=${ARCH}-ranlib
+  export CC=${ARCH}-gcc
+  export NM=${ARCH}-nm
   ./configure --prefix=$tool_chain_path ac_cv_func_malloc_0_nonnull=yes --target=${ARCH} --host=${ARCH}  --enable-static --without-documentation
 fi
-./configure --prefix=$tool_chain_path ac_cv_func_malloc_0_nonnull=yes --target=${ARCH} --host=${ARCH}  --enable-static --without-documentation
 make
 sudo "PATH=$PATH" make install
 sudo rm $tool_chain_path/lib/libmodbus.so*
